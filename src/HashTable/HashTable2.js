af347bfacf3901eb625791d7b11d8ac77b591bf9
zhangguangyu <coolyeah@foxmail.com>
2017-05-31 09:27:46 +0800

    add doc

--- src/HashTable/HashTable2.js
+++ src/HashTable/HashTable2.js
@@ -1,3 +1,11 @@
+/*
+哈希表
+实现思路：
+- string类型的key，将每个字母的ASCII码叠加得出int类型的数字，此数字作为私有变量table数组的下标
+- 私有标量table的每个元素均为链表，哈希表在遇到多个key哈希数字一样的保存对象，保存在链表中
+*/
+
+
 const LinkedList = require('../LinkedList/LinkedList')
 const ValuePair = require('./ValuePair')
 
@@ -7,6 +15,9 @@ class HashTable{
         this.table = [];
     }
 
+    /*
+        本算法的散列函数，hash冲突比较多，作为测试用
+    */
     loseloseHashCode(key){
         if(typeof key !== 'string'){
             return false;
@@ -19,6 +30,16 @@ class HashTable{
         return hash % 37;
     }
 
+    /*
+        最被社区推荐的散列函数之一
+    */
+    djb2HashCode(key){
+        const hash = 5381;
+        for(let i = 0; i < key.length; i++){
+            hash = hash * 33 + key.charCodeAt(i)
+        }
+        return hash % 1013;
+    }
 
     put(key, value){
         let position = this.loseloseHashCode(key);
