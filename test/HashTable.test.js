088f2055516ebf38706cb58338a082c1ef960983
zhangguangyu <coolyeah@foxmail.com>
2017-05-31 19:26:02 +0800

    a test

--- test/HashTable.test.js
+++ test/HashTable.test.js
@@ -2,11 +2,11 @@ const assert = require('assert');
 
 const HashTable = require('../src/HashTable/HashTable2');
 
-it("HashTable's set(), get()", ()=>{
+it("HashTable's set(), get(), remove()", ()=>{
 
     const hashTable = new HashTable();
 
-    const randomNumber = Math.floor(Math.random() * 10);
+    const randomNumber = Math.ceil(Math.random() * 10);
 
     const testObj = {
         a : randomNumber,
@@ -37,4 +37,59 @@ it("HashTable's set(), get()", ()=>{
     assert.equal(AaronV, undefined);
 
 
+})
+
+it("HashTable's performance test, compare with Map", (done)=>{
+    const testObj = {};
+    let startMillisecond,internalMillisecond;
+
+    console.log('start shuffle key');
+    let shufflePool = 'asdsdreffsdawrgwerlrtrahryoysteradfsdfadasawewgkehrwegagoeraibdfhrdsathjeawvzgfbegroxcwinv'; // 用脸滚键盘 :)
+    for(let i = 0; i < 10000; i++){
+        let randomNumber = Math.ceil(Math.random() * 1000);
+        shufflePool += shufflePool.slice(randomNumber, randomNumber*10);
+    }
+
+    for(let i = 0; i < 100; i++){
+        let randomNumber = Math.ceil(Math.random() * 1000);
+        let key = shufflePool.slice(randomNumber, randomNumber*10);
+        randomNumber = Math.ceil(Math.random() * 1000);
+        let value = shufflePool.slice(randomNumber, randomNumber*10);
+        testObj[key] = value;
+    }
+    console.log('end shuffle key');
+
+    const hashTable = new HashTable();
+    startMillisecond = (new Date()).valueOf();
+
+    for(let k in testObj){
+        hashTable.put(k, testObj[k])
+    }
+    internalMillisecond = (new Date()).valueOf() - startMillisecond;
+    console.log(`hashTable put(), spend ${internalMillisecond} ms`);
+
+    startMillisecond = (new Date()).valueOf();
+    for(let k in testObj){
+        let v = hashTable.get(k)
+    }
+    internalMillisecond = (new Date()).valueOf() - startMillisecond;
+    console.log(`hashTable get(), spend ${internalMillisecond} ms`);
+
+
+    const map = new Map();
+    startMillisecond = (new Date()).valueOf();
+    for(let k in testObj){
+        map.set(k, testObj[k])
+    }
+    internalMillisecond = (new Date()).valueOf() - startMillisecond;
+    console.log(`map put(), spend ${internalMillisecond} ms`);
+
+    startMillisecond = (new Date()).valueOf();
+    for(let k in testObj){
+        let v = map.get(k)
+    }
+    internalMillisecond = (new Date()).valueOf() - startMillisecond;
+    console.log(`map get(), spend ${internalMillisecond} ms`);
+
+    done();
 })
